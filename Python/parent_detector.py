1706a0774d01f9c0da43546a74796c27d0bd1727
Reece McKee <rmckee75@charlottesvilleschools.org>
2021-03-01 18:13:50 +0000

    Add test video

--- Python/parent_detector.py
+++ Python/parent_detector.py
@@ -12,17 +12,16 @@ while True:
      print ("Motion Detected!")
      os.chdir("Camera_Videos")
 # Changes directory to Python/Camera_Videos folder, so that videos will be saved there
-     a = datetime.now()
+     time = datetime.now()
+     timestamp = time.strftime("%d_%m_%Y_%H_%M_%S")
 # Finds the current date and time, stores them as a (this serves as a unique identifier for each picture)
-     print ("running...")
-     num = str(a)
 # Convert the date and time to string format
-     video = [ num, ".h264"]
+     video = [timestamp, ".h264"]
 # Create an array with the parts of the eventual name of the picture
      s = "_"
 # The separator in the picture name
      videoname = (s.join(video))
-# Combine the elements of the pic array to form the picname
+# Combine the elements of the video array to form the videoname
      camera.start_recording(videoname)
      PIR.wait_for_no_motion()
      camera.stop_recording()
