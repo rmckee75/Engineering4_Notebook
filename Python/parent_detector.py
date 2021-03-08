02c69d6cdd30c3ec0b062b64a8a2a402ec0204c2
Reece McKee <rmckee75@charlottesvilleschools.org>
2021-03-08 20:02:11 +0000

    Add commented code and converted .mp4 videos

--- Python/parent_detector.py
+++ Python/parent_detector.py
@@ -5,10 +5,15 @@ from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
 import os
+import subprocess
+# Import the relevant libraries and functions
 PIR = MotionSensor(4)
+# The motion detector is plugged into GPIO Pin #4
 camera = PiCamera()
 while True:
+# The motion activated camera runs on an infinite loop
      PIR.wait_for_motion()
+# Do nothing until motion is detected
      print ("Motion Detected!")
      os.chdir("Camera_Videos")
 # Changes directory to Python/Camera_Videos folder, so that videos will be saved there
@@ -17,13 +22,16 @@ while True:
 # Convert the date and time to string format
      video = [timestamp, ".h264"]
      video_mp4 = [timestamp, ".mp4"]
-# Create an array with the parts of the eventual name of the picture
+# Create an array with the parts of the eventual names of the videos
      s = "_"
 # The separator in the picture name
      videoname = s.join(video)
      videoname_mp4 = s.join(video_mp4)
-# Combine the elements of the video array to form the videoname
+# Combine the elements of the video arrays to form the videonames
      camera.start_recording(videoname)
-     PIR.wait_for_no_motion()
+# Start recording a .h264 video
+     camera.wait_recording(10)
+# Record for 10 seconds
      camera.stop_recording()
-     #os.MP4Box -add videoname videoname_mp4
+     subprocess.call(["MP4Box", "-add", videoname, videoname_mp4])
+# Use the MP4Box command in the gpac library to "wrap" the h264 video with a .mp4 video
